65ac45438975d8a0e6b2074297d7e62325d0c9e6
Slava Tovarovsky <117951449+Slava-T-Hub@users.noreply.github.com>
2024-07-31 04:30:06 +0100

    front commit

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const cors = require('cors');
-app.use(cors());
+
 const PORT = 3010;
 
 //=======================================================
